2f16149fb77140b23eb642b126d0290bded59247
Quynh Vo <quynh.vo@mercury.co.nz>
2023-08-20 07:22:11 +1200

    Added new components and used recipe model to create new recipe and add to template using string interpolation and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test',
+      'https://hips.hearstapps.com/hmg-prod/images/delish-202002-pozole-0392-landscape-pf-1582315071.jpg'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'Test',
+      'https://hips.hearstapps.com/hmg-prod/images/delish-202002-pozole-0392-landscape-pf-1582315071.jpg'
+    ),
+  ];
 }
